ed19355a7b8accbe87e406b951aef9a10132ba02
Sergei Kamianetskii <i@ksergey.ru>
2023-05-27 22:06:30 +0300

    Описание компонента для отображения всех заметок

---
+++ client-app/src/components/TableNotesItem.js
@@ -0,0 +1,22 @@
+import NotesRowItem from './NotesRowItem'
+
+export default function TableNotesItem(props) {
+  return (
+    <table className="table table-hover">
+      <thead className="table-light">
+        <tr>
+          <th scope="col">№</th>
+          <th scope="col">Title</th>
+          <th scope="col">Description</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          props.notes.map(item =>
+            <NotesRowItem note={item} />
+          )
+        }
+      </tbody>
+    </table>
+  );
+}
