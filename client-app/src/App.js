994335b03ebe4c419e46a571b01c64f0a6430cb1
Sergei Kamianetskii <i@ksergey.ru>
2023-05-27 21:19:09 +0300

    Базовое представление заметок

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -2,8 +2,43 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div>
+      <div>
+        <div>
+          <h1>Notes</h1>
+        </div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>№</th>
+                <th>Title</th>
+                <th>Description</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>1</th>
+                <th>Title 1</th>
+                <th>Description 1</th>
+              </tr>
+
+              <tr>
+                <th>2</th>
+                <th>Title 2</th>
+                <th>Description 2</th>
+              </tr>
+
+              <tr>
+                <th>3</th>
+                <th>Title 3</th>
+                <th>Description 3</th>
+              </tr>
+
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
