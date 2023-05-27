6a3316ed472d583fa89ff4efb4f954dbd65ff9a4
Sergei Kamianetskii <i@ksergey.ru>
2023-05-27 21:49:46 +0300

    Добавление переменных с данными

--- client-app/src/components/NotesRowItem.js
+++ client-app/src/components/NotesRowItem.js
@@ -1,9 +1,13 @@
 export default function NotesRowItem() {
+  const rowNumber = 1;
+  const rowTitle = "Some title";
+  const rowDescription = "Some description";
+
   return (
     <tr>
-      <th scope="row">#</th>
-      <th>Some title</th>
-      <th>Some description</th>
+      <th scope="row">{rowNumber}</th>
+      <th>{rowTitle}</th>
+      <th>{rowDescription}</th>
     </tr>
   );
 }
