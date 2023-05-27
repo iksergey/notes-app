8486cb5df54fd53c9369d63895128b7957a8b144
Sergei Kamianetskii <i@ksergey.ru>
2023-05-28 01:16:54 +0300

    Добавлен контроллер с чтением и удалением заметок

---
+++ backend/Api/Controllers/NotesController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Api.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class NotesController : ControllerBase
+  {
+    private readonly DataContext dataContext;
+
+    public NotesController(DataContext dataContext)
+    {
+      this.dataContext = dataContext;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Note>>> Get()
+    {
+      return Ok(await dataContext.Notes.ToListAsync());
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Remove(Guid id)
+    {
+      Note note = await dataContext.Notes.FindAsync(id);
+      dataContext.Notes.Remove(note);
+      return Ok(await dataContext.SaveChangesAsync());
+    }
+  }
+}
